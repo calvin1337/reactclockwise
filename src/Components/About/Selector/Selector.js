7715e9a52c1d73bc4232516d36c9b5c1acc8a31b
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-05-14 21:19:46 +0100

    about selector

--- src/Components/About/Selector/Selector.js
+++ src/Components/About/Selector/Selector.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react'
 import "./selector.css"
 
 export class Selector extends Component {
+    
     render() {
         return (
             <div className="linkContainer">
             <div className="testing">
                 <ul>
                     <li class="pageItem activebtn">
-                      <button  href="/">About us</button>
+                      <button href="/">About us</button>
                     </li>
                     <li>
                         //
                     </li>
 
                  <li class="pageItem">
-                <button href="/">Facilites</button>
+                <button onClick={() => this.props.currentpage("facilites")} href="/">Facilites</button>
                 </li>
                 </ul>
   </div>
--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -4,11 +4,19 @@ import AboutIntro from '../../Components/Homepage/AboutIntro'
 import Selector from '../../Components/About/Selector/Selector'
 
 export class About extends Component {
+    state = {
+        activepage: "about"
+    }
+
+    pageSelector = (testing) => {
+            this.setState({activepage: testing})
+    }
+
     render() {
         return (
             <div>
                  <Header />
-                  <Selector />
+                  <Selector activepage={this.state.activepage} currentpage={(testing) => this.pageSelector(testing)} />
                  <AboutIntro />
                  
             </div>     
